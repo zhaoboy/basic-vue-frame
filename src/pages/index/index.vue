2d21d94e4b70720c9df66e549459999a40835958
Zhaoboy <zhaoboy@outlook.com>
2019-12-17 09:38:37 +0800

    修改vuex的引用

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,17 @@
 <template>
   <el-container>
     <el-header>
-      <header-nav :activeHeaderIndex='activeHeaderIndex'></header-nav>
+      <header-nav :activeHeaderIndex='activeHeaderIndex' v-if="headerShow"></header-nav>
     </el-header>
     <el-container>
       <el-aside width="200px">
         <side-bar></side-bar>
       </el-aside>
       <el-container>
-        <el-main>Main</el-main>
+        <el-main>
+          <button @click="showHeaderMethod()">Show</button>
+          <button @click="hideHeaderMethod()">Hide</button>
+        </el-main>
         <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
@@ -17,8 +20,45 @@
 <script>
 import headerNav from '@components/common/headerNav.vue';
 import sideBar from '@components/common/sideBar.vue';
+import Vue from 'vue';
+import Vuex from 'vuex';
 import { mapGetters, mapActions } from 'vuex';
+Vue.use(Vuex);
+
+const state = { //要设置的全局访问的state对象
+  isShowHeader: true,
+  changableNum:0
+};
+const getters = {   //实时监听state值的变化(最新状态)
+  isHeaderShow(state) {  //方法名随意,主要是来承载变化的showFooter的值
+    return state.isShowHeader;
+  },
+};
+const mutations = {
+  showHeader(state) {   //自定义改变state初始值的方法，这里面的参数除了state之外还可以再传额外的参数(变量或对象);
+    state.isShowHeader = true;
+  },
+  hideHeader(state) {  //同上
+    state.isShowHeader = false;
+  },
+};
+const actions = {
+  hideHeaderAction(context) {  //自定义触发mutations里函数的方法，context与store 实例具有相同方法和属性
+    context.commit('hideHeader');
+  },
+  showHeaderAction(context) {  //同上注释
+    context.commit('showHeader');
+  },
+};
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
+
 export default {
+  store,
   name: 'index',
   components: {
     'header-nav': headerNav,
@@ -27,15 +67,25 @@ export default {
   data() {
     return {
       activeHeaderIndex: '1',
+      // isShowHeader: this.$store.getters.isHeaderShow,
     };
   },
   computed: {
+    headerShow(){
+      return this.$store.getters.isHeaderShow;
+    },
     ...mapGetters(['count'])
   },
   methods: {
     ...mapActions([
       'addCount'
     ]),
+    showHeaderMethod() {
+      this.$store.dispatch('showHeaderAction');
+    },
+    hideHeaderMethod() {
+      this.$store.dispatch('hideHeaderAction');
+    },
     add(){
       this.addCount({num: 10});
     }
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import app from './modules/app';
-import user from './modules/user';
-import getters from './getters';
+// import app from './modules/app';
+// import user from './modules/user';
+// import getters from './getters';
 
 Vue.use(Vuex);
-
-const store = new Vuex.Store({
-  modules: {
-    app,
-    user
-  },
-  getters
-});
+const store = new Vuex.Store();
+// const store = new Vuex.Store({
+//   modules: {
+//     app,
+//     user
+//   },
+//   getters
+// });
 
 export default store;
